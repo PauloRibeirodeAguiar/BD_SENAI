e52b8d8caeef73c890f1644fe151bd0334ed058f
PauloRibeirodeAguiar <paulodeaguiar@gmail.com>
2025-03-24 16:46:05 -0300

    BD_SENAI

---
+++ INSERT21032025.sql
@@ -0,0 +1,28 @@
+insert into departamento values
+(null, "Financeiro"),
+(null, "Recursos Humanos"),
+(null, "Marketing"),
+(null, "Vendas");
+
+INSERT INTO Empregado (NomeEmpregado, SalarioEmpregado, SexoEmpregado, FkDepartamento)
+VALUES
+('Carlos Silva', 3000.00, 'M', 1),
+('Maria Oliveira', 3500.00, 'F', 2),
+('João Pereira', 2800.00, 'M', 3),
+('Ana Costa', 4000.00, 'F', 1),
+('Lucas Souza', 2500.00, 'M', 2),
+('Beatriz Almeida', 3200.00, 'F', 3),
+('Pedro Santos', 4500.00, 'M', 1),
+('Fernanda Lima', 3700.00, 'F', 2),
+('Ricardo Barbosa', 2900.00, 'M', 3),
+('Juliana Rodrigues', 3300.00, 'F', 1),
+('Felipe Gomes', 3100.00, 'M', 2),
+('Mariana Pereira', 3600.00, 'F', 3),
+('José Martins', 3400.00, 'M', 1),
+('Patrícia Ferreira', 3800.00, 'F', 2),
+('Eduardo Ribeiro', 3000.00, 'M', 3),
+('Isabela Silva', 3300.00, 'F', 1),
+('Gustavo Costa', 3100.00, 'M', 2),
+('Camila Oliveira', 3400.00, 'F', 3),
+('Leandro Mendes', 3200.00, 'M', 1),
+('Aline Santos', 3500.00, 'F', 2);
